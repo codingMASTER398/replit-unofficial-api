e3c06f251d883dd6133ca2e9397dcd7dc231a7bf
A person somewhere that does something for the reason that he likes to do that thing. I really don't know what I'm saying, but I do know I can make this name 255 characters, so I'm just going to put a bunch of "a"s until I get to 255 aaaaaaaaaaaaaaaaaaaaa <55938439+codingMASTER398@users.noreply.github.com>
2021-11-21 15:20:56 +1100

    Add files via upload

--- index.js
+++ index.js
@@ -135,7 +135,37 @@ function search(username,repl,next) {
 function repldata(username,repl) {
     return new Promise(async function(resolve,reject) {
         search(username,repl).then(function(r) {
-            resolve(r)
+            var heads = globalheaders
+            heads['path'] = '/graphql'
+            heads['method'] = 'POST'
+            heads['referer'] = 'https://replit.com/@'+username
+
+            var bodily = JSON.stringify([
+                {
+                  "operationName": "ExploreReplView",
+                  "variables": {
+                    "replId": r.id
+                  },
+                  "query": "query ExploreReplView($replId: String!) {\n  repl(id: $replId) {\n    ... on Repl {\n      id\n      ...ReplViewRepl\n      __typename\n    }\n    __typename\n  }\n  currentUser {\n    id\n    ...ReplViewCurrentUser\n    __typename\n  }\n}\n\nfragment ReplViewRepl on Repl {\n  id\n  title\n  timeCreated\n  imageUrl\n  iconUrl\n  publicReleasesForkCount\n  publicForkCount\n  owner {\n    ... on Team {\n      id\n      username\n      url\n      image\n      __typename\n    }\n    ... on User {\n      id\n      username\n      url\n      image\n      __typename\n    }\n    __typename\n  }\n  derivedApps(count: 3) {\n    items {\n      id\n      ...ExploreReplCardRepl\n      __typename\n    }\n    __typename\n  }\n  relatedRepls(limitPerGroup: 3) {\n    name\n    repls {\n      id\n      publishedAs\n      ...ReplLinkRepl\n      ...ExploreReplCardRepl\n      ...TemplateReplCardRepl\n      __typename\n    }\n    __typename\n  }\n  lang {\n    id\n    displayName\n    __typename\n  }\n  currentUserPermissions {\n    containerWrite\n    publish\n    __typename\n  }\n  publishedAs\n  deployment {\n    id\n    activeRelease {\n      id\n      timeCreated\n      __typename\n    }\n    __typename\n  }\n  ...ReplViewReplViewerRepl\n  ...ReplLinkRepl\n  ...ReplViewFooterRepl\n  __typename\n}\n\nfragment ExploreReplCardRepl on Repl {\n  id\n  title\n  runCount\n  likeCount\n  imageUrl\n  url\n  description(plainText: true)\n  tags {\n    id\n    __typename\n  }\n  owner {\n    ... on User {\n      id\n      username\n      image\n      url\n      __typename\n    }\n    ... on Team {\n      id\n      username\n      image\n      url\n      __typename\n    }\n    __typename\n  }\n  multiplayers {\n    id\n    username\n    image\n    url\n    __typename\n  }\n  __typename\n}\n\nfragment ReplLinkRepl on Repl {\n  id\n  url\n  nextPagePathname\n  __typename\n}\n\nfragment TemplateReplCardRepl on Repl {\n  id\n  iconUrl\n  templateCategory\n  title\n  description(plainText: true)\n  publicReleasesForkCount\n  likeCount\n  ...ReplLinkRepl\n  owner {\n    ... on User {\n      id\n      ...TemplateReplCardFooterUser\n      __typename\n    }\n    ... on Team {\n      id\n      ...TemplateReplCardFooterTeam\n      __typename\n    }\n    __typename\n  }\n  __typename\n}\n\nfragment TemplateReplCardFooterUser on User {\n  id\n  username\n  image\n  url\n  __typename\n}\n\nfragment TemplateReplCardFooterTeam on Team {\n  id\n  username\n  image\n  url\n  __typename\n}\n\nfragment ReplViewReplViewerRepl on Repl {\n  id\n  publishedAs\n  runCount\n  prodUrl: hostedUrl(dotty: true)\n  isProject\n  lang {\n    id\n    header\n    displayName\n    __typename\n  }\n  ...ReplViewerOutputOverlayRepl\n  ...UseReplViewerRepl\n  ...LikeButtonRepl\n  __typename\n}\n\nfragment ReplViewerOutputOverlayRepl on Repl {\n  id\n  title\n  imageUrl\n  lastPublishedAt\n  currentUserPermissions {\n    changeImageUrl\n    __typename\n  }\n  __typename\n}\n\nfragment UseReplViewerRepl on Repl {\n  id\n  devUrl: hostedUrl\n  url\n  lang {\n    id\n    canUseShellRunner\n    hasReplboxWebview\n    __typename\n  }\n  config {\n    isServer\n    isVnc\n    __typename\n  }\n  deployment {\n    id\n    activeRelease {\n      id\n      hostedUrl\n      __typename\n    }\n    __typename\n  }\n  ...CrosisContextRepl\n  __typename\n}\n\nfragment CrosisContextRepl on Repl {\n  id\n  language\n  slug\n  user {\n    id\n    username\n    __typename\n  }\n  currentUserPermissions {\n    containerWrite\n    __typename\n  }\n  flagOwnerDotReplitPackager: gateOnOwner(feature: \"flag-dotreplit-packager\")\n  __typename\n}\n\nfragment LikeButtonRepl on Repl {\n  id\n  currentUserDidLike\n  likeCount\n  url\n  __typename\n}\n\nfragment ReplViewFooterRepl on Repl {\n  id\n  description\n  lastPublishedAt\n  publishedAs\n  deployment {\n    id\n    activeRelease {\n      id\n      timeCreated\n      __typename\n    }\n    __typename\n  }\n  owner {\n    ... on Team {\n      id\n      username\n      url\n      image\n      __typename\n    }\n    ... on User {\n      id\n      username\n      url\n      image\n      __typename\n    }\n    __typename\n  }\n  source {\n    release {\n      id\n      __typename\n    }\n    deployment {\n      id\n      repl {\n        id\n        ...ReplViewSourceRepl\n        __typename\n      }\n      __typename\n    }\n    __typename\n  }\n  tags {\n    id\n    __typename\n  }\n  origin {\n    id\n    ...ReplViewSourceRepl\n    __typename\n  }\n  __typename\n}\n\nfragment ReplViewSourceRepl on Repl {\n  id\n  title\n  templateLabel\n  ...ReplLinkRepl\n  owner {\n    ... on Team {\n      id\n      username\n      url\n      image\n      __typename\n    }\n    ... on User {\n      id\n      username\n      url\n      image\n      __typename\n    }\n    __typename\n  }\n  __typename\n}\n\nfragment ReplViewCurrentUser on CurrentUser {\n  id\n  username\n  isSubscribed\n  ...ReplViewReplViewerCurrentUser\n  __typename\n}\n\nfragment ReplViewReplViewerCurrentUser on CurrentUser {\n  id\n  ...LikeButtonCurrentUser\n  ...CrosisContextCurrentUser\n  __typename\n}\n\nfragment LikeButtonCurrentUser on CurrentUser {\n  id\n  isVerified\n  __typename\n}\n\nfragment CrosisContextCurrentUser on CurrentUser {\n  id\n  username\n  isSubscribed\n  flagTrackOtClientDataLoss: gate(feature: \"flag-ot-data-loss-client-tracking\")\n  __typename\n}\n"
+                }
+              ])
+            fetch("https://replit.com/graphql", {
+                "headers": heads,
+                "referrer": 'https://replit.com/@'+username,
+                "referrerPolicy": "strict-origin-when-cross-origin",
+                "body": bodily,
+                "method": "POST",
+                "mode": "cors"
+            }).then(async function response(o) {
+                o = await o.json()
+                if(o[0].data.repl){
+                    resolve(o[0].data.repl)
+                }else{
+                    reject("there was an error while getting repl info")
+                }
+            }).catch(function(err) {
+                reject("there was an error while getting repl info")
+            })
         }).catch(function(err) {
             if(err == "no"){
                 reject("Repl or user does not exist")
